1b63fbc7e1288da0a1a82a719c1bdb76ee1532ad
AnyaIsabelleMCorpin <anyaisabelle.corpin@smc.pshs.edu.ph>
2025-10-27 10:49:40 +0800

    Add files via upload

---
+++ Sum_of_numbers_to_your_age.py
@@ -0,0 +1,6 @@
+age = int(input("Hi there! Please enter our age: "))
+total_sum = 0
+for a in range (1, age + 1):
+    total_sum =  total_sum + a 
+
+print("The sum of all numbers from 1 to", age, "is:", total_sum )
